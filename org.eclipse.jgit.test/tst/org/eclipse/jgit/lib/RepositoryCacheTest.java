eclipse-jgit/jgit

Repository can be configured with FS
On Windows, FS_Win32_Cygwin has been used if a Cygwin Git installation
is present in the PATH. Assuming that the user works with the Cygwin
Git installation may result in unnecessary overhead if he actually
does not.

Applications built on top of jgit may have more knowledge on the
actually used Git client (Cygwin or not) and hence should be able to
configure which FS to use accordingly.

org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/RepositoryCacheTest.java
@@ -54,13 +54,13 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 		File gitdir = db.getDirectory();
 		File parent = gitdir.getParentFile();
 		File other = new File(parent, "notagit");
-		assertEquals(gitdir, FileKey.exact(gitdir).getFile());
-		assertEquals(parent, FileKey.exact(parent).getFile());
-		assertEquals(other, FileKey.exact(other).getFile());
+		assertEquals(gitdir, FileKey.exact(gitdir, db.getFs()).getFile());
+		assertEquals(parent, FileKey.exact(parent, db.getFs()).getFile());
+		assertEquals(other, FileKey.exact(other, db.getFs()).getFile());
 
-		assertEquals(gitdir, FileKey.lenient(gitdir).getFile());
-		assertEquals(gitdir, FileKey.lenient(parent).getFile());
-		assertEquals(other, FileKey.lenient(other).getFile());
+		assertEquals(gitdir, FileKey.lenient(gitdir, db.getFs()).getFile());
+		assertEquals(gitdir, FileKey.lenient(parent, db.getFs()).getFile());
+		assertEquals(other, FileKey.lenient(other, db.getFs()).getFile());
 	}
 
 	public void testBareFileKey() throws IOException {
@@ -71,21 +71,21 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 		assertTrue(name.endsWith(".git"));
 		name = name.substring(0, name.length() - 4);
 
-		assertEquals(gitdir, FileKey.exact(gitdir).getFile());
+		assertEquals(gitdir, FileKey.exact(gitdir, db.getFs()).getFile());
 
-		assertEquals(gitdir, FileKey.lenient(gitdir).getFile());
-		assertEquals(gitdir, FileKey.lenient(new File(parent, name)).getFile());
+		assertEquals(gitdir, FileKey.lenient(gitdir, db.getFs()).getFile());
+		assertEquals(gitdir, FileKey.lenient(new File(parent, name), db.getFs()).getFile());
 	}
 
 	public void testFileKeyOpenExisting() throws IOException {
 		Repository r;
 
-		r = new FileKey(db.getDirectory()).open(true);
+		r = new FileKey(db.getDirectory(), db.getFs()).open(true);
 		assertNotNull(r);
 		assertEquals(db.getDirectory(), r.getDirectory());
 		r.close();
 
-		r = new FileKey(db.getDirectory()).open(false);
+		r = new FileKey(db.getDirectory(), db.getFs()).open(false);
 		assertNotNull(r);
 		assertEquals(db.getDirectory(), r.getDirectory());
 		r.close();
@@ -99,13 +99,13 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 		assertFalse(gitdir.exists());
 
 		try {
-			new FileKey(gitdir).open(true);
+			new FileKey(gitdir, db.getFs()).open(true);
 			fail("incorrectly opened a non existant repository");
 		} catch (RepositoryNotFoundException e) {
 			assertEquals("repository not found: " + gitdir, e.getMessage());
 		}
 
-		final Repository o = new FileKey(gitdir).open(false);
+		final Repository o = new FileKey(gitdir, db.getFs()).open(false);
 		assertNotNull(o);
 		assertEquals(gitdir, o.getDirectory());
 		assertFalse(gitdir.exists());
@@ -114,18 +114,18 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 	public void testCacheRegisterOpen() throws Exception {
 		final File dir = db.getDirectory();
 		RepositoryCache.register(db);
-		assertSame(db, RepositoryCache.open(FileKey.exact(dir)));
+		assertSame(db, RepositoryCache.open(FileKey.exact(dir, db.getFs())));
 
 		assertEquals(".git", dir.getName());
 		final File parent = dir.getParentFile();
-		assertSame(db, RepositoryCache.open(FileKey.lenient(parent)));
+		assertSame(db, RepositoryCache.open(FileKey.lenient(parent, db.getFs())));
 	}
 
 	public void testCacheOpen() throws Exception {
-		final FileKey loc = FileKey.exact(db.getDirectory());
+		final FileKey loc = FileKey.exact(db.getDirectory(), db.getFs());
 		final Repository d2 = RepositoryCache.open(loc);
 		assertNotSame(db, d2);
-		assertSame(d2, RepositoryCache.open(FileKey.exact(loc.getFile())));
+		assertSame(d2, RepositoryCache.open(FileKey.exact(loc.getFile(), db.getFs())));
 		d2.close();
 		d2.close();
 	}

org.eclipse.jgit.test/tst/org/eclipse/jgit/treewalk/FileTreeIteratorTest.java
@@ -77,7 +77,7 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 	public void testEmptyIfRootIsFile() throws Exception {
 		final File r = new File(trash, paths[0]);
 		assertTrue(r.isFile());
-		final FileTreeIterator fti = new FileTreeIterator(r);
+		final FileTreeIterator fti = new FileTreeIterator(r, db.getFs());
 		assertTrue(fti.first());
 		assertTrue(fti.eof());
 	}
@@ -85,7 +85,7 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 	public void testEmptyIfRootDoesNotExist() throws Exception {
 		final File r = new File(trash, "not-existing-file");
 		assertFalse(r.exists());
-		final FileTreeIterator fti = new FileTreeIterator(r);
+		final FileTreeIterator fti = new FileTreeIterator(r, db.getFs());
 		assertTrue(fti.first());
 		assertTrue(fti.eof());
 	}
@@ -96,13 +96,13 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 		r.mkdir();
 		assertTrue(r.isDirectory());
 
-		final FileTreeIterator fti = new FileTreeIterator(r);
+		final FileTreeIterator fti = new FileTreeIterator(r, db.getFs());
 		assertTrue(fti.first());
 		assertTrue(fti.eof());
 	}
 
 	public void testSimpleIterate() throws Exception {
-		final FileTreeIterator top = new FileTreeIterator(trash);
+		final FileTreeIterator top = new FileTreeIterator(trash, db.getFs());
 
 		assertTrue(top.first());
 		assertFalse(top.eof());
@@ -149,7 +149,7 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 	}
 
 	public void testComputeFileObjectId() throws Exception {
-		final FileTreeIterator top = new FileTreeIterator(trash);
+		final FileTreeIterator top = new FileTreeIterator(trash, db.getFs());
 
 		final MessageDigest md = Constants.newMessageDigest();
 		md.update(Constants.encodeASCII(Constants.TYPE_BLOB));

rtyley/jgit-git-lfs-demo

Change-Id: Ifc4278078b298781d55cf5421e9647a21fa5db24
Former-commit-id: 936e4ab2f2ad4847e22483b26eb61f0c8e56b70c

org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/RepositoryCacheTest.java
@@ -54,13 +54,13 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 		File gitdir = db.getDirectory();
 		File parent = gitdir.getParentFile();
 		File other = new File(parent, "notagit");
-		assertEquals(gitdir, FileKey.exact(gitdir).getFile());
-		assertEquals(parent, FileKey.exact(parent).getFile());
-		assertEquals(other, FileKey.exact(other).getFile());
+		assertEquals(gitdir, FileKey.exact(gitdir, db.getFS()).getFile());
+		assertEquals(parent, FileKey.exact(parent, db.getFS()).getFile());
+		assertEquals(other, FileKey.exact(other, db.getFS()).getFile());
 
-		assertEquals(gitdir, FileKey.lenient(gitdir).getFile());
-		assertEquals(gitdir, FileKey.lenient(parent).getFile());
-		assertEquals(other, FileKey.lenient(other).getFile());
+		assertEquals(gitdir, FileKey.lenient(gitdir, db.getFS()).getFile());
+		assertEquals(gitdir, FileKey.lenient(parent, db.getFS()).getFile());
+		assertEquals(other, FileKey.lenient(other, db.getFS()).getFile());
 	}
 
 	public void testBareFileKey() throws IOException {
@@ -71,21 +71,21 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 		assertTrue(name.endsWith(".git"));
 		name = name.substring(0, name.length() - 4);
 
-		assertEquals(gitdir, FileKey.exact(gitdir).getFile());
+		assertEquals(gitdir, FileKey.exact(gitdir, db.getFS()).getFile());
 
-		assertEquals(gitdir, FileKey.lenient(gitdir).getFile());
-		assertEquals(gitdir, FileKey.lenient(new File(parent, name)).getFile());
+		assertEquals(gitdir, FileKey.lenient(gitdir, db.getFS()).getFile());
+		assertEquals(gitdir, FileKey.lenient(new File(parent, name), db.getFS()).getFile());
 	}
 
 	public void testFileKeyOpenExisting() throws IOException {
 		Repository r;
 
-		r = new FileKey(db.getDirectory()).open(true);
+		r = new FileKey(db.getDirectory(), db.getFS()).open(true);
 		assertNotNull(r);
 		assertEquals(db.getDirectory(), r.getDirectory());
 		r.close();
 
-		r = new FileKey(db.getDirectory()).open(false);
+		r = new FileKey(db.getDirectory(), db.getFS()).open(false);
 		assertNotNull(r);
 		assertEquals(db.getDirectory(), r.getDirectory());
 		r.close();
@@ -99,13 +99,13 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 		assertFalse(gitdir.exists());
 
 		try {
-			new FileKey(gitdir).open(true);
+			new FileKey(gitdir, db.getFS()).open(true);
 			fail("incorrectly opened a non existant repository");
 		} catch (RepositoryNotFoundException e) {
 			assertEquals("repository not found: " + gitdir, e.getMessage());
 		}
 
-		final Repository o = new FileKey(gitdir).open(false);
+		final Repository o = new FileKey(gitdir, db.getFS()).open(false);
 		assertNotNull(o);
 		assertEquals(gitdir, o.getDirectory());
 		assertFalse(gitdir.exists());
@@ -114,18 +114,18 @@ public class RepositoryCacheTest extends RepositoryTestCase {
 	public void testCacheRegisterOpen() throws Exception {
 		final File dir = db.getDirectory();
 		RepositoryCache.register(db);
-		assertSame(db, RepositoryCache.open(FileKey.exact(dir)));
+		assertSame(db, RepositoryCache.open(FileKey.exact(dir, db.getFS())));
 
 		assertEquals(".git", dir.getName());
 		final File parent = dir.getParentFile();
-		assertSame(db, RepositoryCache.open(FileKey.lenient(parent)));
+		assertSame(db, RepositoryCache.open(FileKey.lenient(parent, db.getFS())));
 	}
 
 	public void testCacheOpen() throws Exception {
-		final FileKey loc = FileKey.exact(db.getDirectory());
+		final FileKey loc = FileKey.exact(db.getDirectory(), db.getFS());
 		final Repository d2 = RepositoryCache.open(loc);
 		assertNotSame(db, d2);
-		assertSame(d2, RepositoryCache.open(FileKey.exact(loc.getFile())));
+		assertSame(d2, RepositoryCache.open(FileKey.exact(loc.getFile(), db.getFS())));
 		d2.close();
 		d2.close();
 	}

org.eclipse.jgit.test/tst/org/eclipse/jgit/treewalk/FileTreeIteratorTest.java
@@ -77,7 +77,7 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 	public void testEmptyIfRootIsFile() throws Exception {
 		final File r = new File(trash, paths[0]);
 		assertTrue(r.isFile());
-		final FileTreeIterator fti = new FileTreeIterator(r);
+		final FileTreeIterator fti = new FileTreeIterator(r, db.getFS());
 		assertTrue(fti.first());
 		assertTrue(fti.eof());
 	}
@@ -85,7 +85,7 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 	public void testEmptyIfRootDoesNotExist() throws Exception {
 		final File r = new File(trash, "not-existing-file");
 		assertFalse(r.exists());
-		final FileTreeIterator fti = new FileTreeIterator(r);
+		final FileTreeIterator fti = new FileTreeIterator(r, db.getFS());
 		assertTrue(fti.first());
 		assertTrue(fti.eof());
 	}
@@ -96,13 +96,13 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 		r.mkdir();
 		assertTrue(r.isDirectory());
 
-		final FileTreeIterator fti = new FileTreeIterator(r);
+		final FileTreeIterator fti = new FileTreeIterator(r, db.getFS());
 		assertTrue(fti.first());
 		assertTrue(fti.eof());
 	}
 
 	public void testSimpleIterate() throws Exception {
-		final FileTreeIterator top = new FileTreeIterator(trash);
+		final FileTreeIterator top = new FileTreeIterator(trash, db.getFS());
 
 		assertTrue(top.first());
 		assertFalse(top.eof());
@@ -149,7 +149,7 @@ public class FileTreeIteratorTest extends RepositoryTestCase {
 	}
 
 	public void testComputeFileObjectId() throws Exception {
-		final FileTreeIterator top = new FileTreeIterator(trash);
+		final FileTreeIterator top = new FileTreeIterator(trash, db.getFS());
 
 		final MessageDigest md = Constants.newMessageDigest();
 		md.update(Constants.encodeASCII(Constants.TYPE_BLOB));