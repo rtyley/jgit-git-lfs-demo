eclipse-jgit/jgit

Execute ReceiveCommands via a method rather than in ReceivePack
This allows a PreReceiveHook to easily "take over" all of the
ReceiveCommands passed to it, preventing any of them from being handled
within the ReceivePack core.

Change-Id: I2a8c1fc44e8dcadf22cd97a8ec4ee79d4d9d08f1

org.eclipse.jgit/src/org/eclipse/jgit/transport/ReceiveCommand.java
@@ -43,11 +43,16 @@
 
 package org.eclipse.jgit.transport;
 
+import java.io.IOException;
+import java.text.MessageFormat;
+
+import org.eclipse.jgit.JGitText;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefUpdate;
 
 /**
- * A command being processed by {@link ReceivePack}.
+ * A command being processed by {@link ReceiveSession}.
  * <p>
  * This command instance roughly translates to the server side representation of
  * the {@link RemoteRefUpdate} created by the client.
@@ -130,7 +135,7 @@ public class ReceiveCommand {
 	private String message;
 
 	/**
-	 * Create a new command for {@link ReceivePack}.
+	 * Create a new command for {@link ReceiveSession}.
 	 *
 	 * @param oldId
 	 *            the old object id; must not be null. Use
@@ -213,12 +218,85 @@ public class ReceiveCommand {
 		message = m;
 	}
 
+	/**
+	 * Execute this command during a receive-pack session.
+	 * <p>
+	 * Sets the status of the command as a side effect.
+	 *
+	 * @param rs
+	 *            receive-pack session.
+	 */
+	public void execute(final ReceiveSession rs) {
+		try {
+			final RefUpdate ru = rs.getRepository().updateRef(getRefName());
+			ru.setRefLogIdent(rs.getRefLogIdent());
+			switch (getType()) {
+			case DELETE:
+				if (!ObjectId.zeroId().equals(getOldId())) {
+					// We can only do a CAS style delete if the client
+					// didn't bork its delete request by sending the
+					// wrong zero id rather than the advertised one.
+					//
+					ru.setExpectedOldObjectId(getOldId());
+				}
+				ru.setForceUpdate(true);
+				setResult(ru.delete(rs.getRevWalk()));
+				break;
+
+			case CREATE:
+			case UPDATE:
+			case UPDATE_NONFASTFORWARD:
+				ru.setForceUpdate(rs.isAllowNonFastForwards());
+				ru.setExpectedOldObjectId(getOldId());
+				ru.setNewObjectId(getNewId());
+				ru.setRefLogMessage("push", true);
+				setResult(ru.update(rs.getRevWalk()));
+				break;
+			}
+		} catch (IOException err) {
+			setResult(Result.REJECTED_OTHER_REASON, MessageFormat.format(
+					JGitText.get().lockError, err.getMessage()));
+		}
+	}
+
 	void setRef(final Ref r) {
 		ref = r;
 	}
 
 	void setType(final Type t) {
 		type = t;
+	}
+
+	private void setResult(final RefUpdate.Result r) {
+		switch (r) {
+		case NOT_ATTEMPTED:
+			setResult(Result.NOT_ATTEMPTED);
+			break;
+
+		case LOCK_FAILURE:
+		case IO_FAILURE:
+			setResult(Result.LOCK_FAILURE);
+			break;
+
+		case NO_CHANGE:
+		case NEW:
+		case FORCED:
+		case FAST_FORWARD:
+			setResult(Result.OK);
+			break;
+
+		case REJECTED:
+			setResult(Result.REJECTED_NONFASTFORWARD);
+			break;
+
+		case REJECTED_CURRENT_BRANCH:
+			setResult(Result.REJECTED_CURRENT_BRANCH);
+			break;
+
+		default:
+			setResult(Result.REJECTED_OTHER_REASON, r.name());
+			break;
+		}
 	}
 
 	@Override

rtyley/jgit-git-lfs-demo

Former-commit-id: 4bf22ff6e840b190e0a019e86cce19001dc587f4

org.eclipse.jgit/src/org/eclipse/jgit/transport/ReceiveCommand.java
@@ -43,8 +43,13 @@
 
 package org.eclipse.jgit.transport;
 
+import java.io.IOException;
+import java.text.MessageFormat;
+
+import org.eclipse.jgit.JGitText;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefUpdate;
 
 /**
  * A command being processed by {@link ReceivePack}.
@@ -213,12 +218,85 @@ public class ReceiveCommand {
 		message = m;
 	}
 
+	/**
+	 * Execute this command during a receive-pack session.
+	 * <p>
+	 * Sets the status of the command as a side effect.
+	 *
+	 * @param rp
+	 *            receive-pack session.
+	 */
+	public void execute(final ReceivePack rp) {
+		try {
+			final RefUpdate ru = rp.getRepository().updateRef(getRefName());
+			ru.setRefLogIdent(rp.getRefLogIdent());
+			switch (getType()) {
+			case DELETE:
+				if (!ObjectId.zeroId().equals(getOldId())) {
+					// We can only do a CAS style delete if the client
+					// didn't bork its delete request by sending the
+					// wrong zero id rather than the advertised one.
+					//
+					ru.setExpectedOldObjectId(getOldId());
+				}
+				ru.setForceUpdate(true);
+				setResult(ru.delete(rp.getRevWalk()));
+				break;
+
+			case CREATE:
+			case UPDATE:
+			case UPDATE_NONFASTFORWARD:
+				ru.setForceUpdate(rp.isAllowNonFastForwards());
+				ru.setExpectedOldObjectId(getOldId());
+				ru.setNewObjectId(getNewId());
+				ru.setRefLogMessage("push", true);
+				setResult(ru.update(rp.getRevWalk()));
+				break;
+			}
+		} catch (IOException err) {
+			setResult(Result.REJECTED_OTHER_REASON, MessageFormat.format(
+					JGitText.get().lockError, err.getMessage()));
+		}
+	}
+
 	void setRef(final Ref r) {
 		ref = r;
 	}
 
 	void setType(final Type t) {
 		type = t;
+	}
+
+	private void setResult(final RefUpdate.Result r) {
+		switch (r) {
+		case NOT_ATTEMPTED:
+			setResult(Result.NOT_ATTEMPTED);
+			break;
+
+		case LOCK_FAILURE:
+		case IO_FAILURE:
+			setResult(Result.LOCK_FAILURE);
+			break;
+
+		case NO_CHANGE:
+		case NEW:
+		case FORCED:
+		case FAST_FORWARD:
+			setResult(Result.OK);
+			break;
+
+		case REJECTED:
+			setResult(Result.REJECTED_NONFASTFORWARD);
+			break;
+
+		case REJECTED_CURRENT_BRANCH:
+			setResult(Result.REJECTED_CURRENT_BRANCH);
+			break;
+
+		default:
+			setResult(Result.REJECTED_OTHER_REASON, r.name());
+			break;
+		}
 	}
 
 	@Override